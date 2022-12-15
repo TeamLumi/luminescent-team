ab1fe9e4325057e3bd4fbf9f5f7c110441da5849
Eevenin <73308769+biomagnet@users.noreply.github.com>
2022-12-14 23:10:16 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,6 +70,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      docs: {
+      sidebar: {
+        hideable: true,
+      },
+    },
       navbar: {
         title: 'Team Luminescent',
         logo: {
