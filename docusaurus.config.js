699e6110ef91b3121514a66b4095e24be286561a
Jack Footner <jackrfootner@gmail.com>
2022-12-18 17:32:45 +1030

    luminescent.team

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Luminescent Version',
   tagline: 'A BDSP ROM Hack',
-  url: 'https://lumi.mrhe.ro',
+  url: 'https://luminescent.team',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
