1ab924364be33954bbe9dd48e8d8bf84f9f4d355
Mr Hero <Jackrfootner@gmail.com>
2022-12-21 20:05:58 +1030

    Disable Paginator on specific ids

---
+++ src/theme/DocItem/Paginator/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Paginator from '@theme-original/DocItem/Paginator';
+import {useLocation} from '@docusaurus/router';
+
+const disabledBases = [
+  "docs"
+]
+
+export default function PaginatorWrapper(props) {
+  const location = useLocation();
+  console.log(location);
+
+  // Check each disabled base and return null if in list
+  for (let i = 0; i < disabledBases.length; i++) {
+    const base = disabledBases[i];
+    if (location.pathname.startsWith(`/${base}`)) {
+      return null;
+    }
+  }
+
+  // Otherwise return normally
+  return (
+    <>
+      <Paginator {...props} />
+    </>
+  );
+}
