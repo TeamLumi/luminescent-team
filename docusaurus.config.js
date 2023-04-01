2dda0a3c32a2b85d4c5346b9746f92732abbbcb9
Eevenin <73308769+biomagnet@users.noreply.github.com>
2023-04-01 14:00:04 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Luminescent Version',
+  title: 'Luminescent Platinum',
   tagline: 'A BDSP ROM Hack',
   url: 'https://luminescent.team',
   baseUrl: '/',
