a9ef11fdf2a3f060b3d0c5a1c4fc4da62cdefad6
Phuc Tran <glup3.tran@gmail.com>
2023-04-20 11:28:14 +0200

    create responsive stats component

--- src/components/Pokedex/index.js
+++ src/components/Pokedex/index.js
@@ -2,11 +2,10 @@ import React, { useState } from 'react';
 import style from './styles.module.css';
 import { Grid, Typography, Avatar } from '@mui/material';
 import { getPokemonInfo, getPokemonLearnset, getEggMoves } from '../../../dexUtils';
-import BaseStatCalculations from './baseStatCalculations';
-import BaseStatsList from './baseStatsList';
 import Type from './type';
 import PokedexAccordion from './pokedexAccordion';
 import EvolutionGraph from './evolutionGraph';
+import { PokemonStats } from './PokemonStats/PokemonStats';
 
 export default function PokedexFeatures(props) {
   const pokemonInfo = getPokemonInfo(props.dexId ?? 0);
@@ -17,8 +16,8 @@ export default function PokedexFeatures(props) {
 
   const learnset = getPokemonLearnset(props.dexId);
   const moveList = [];
-  for(let i = 0; i < learnset.length; i += 2) {
-    moveList.push({level: learnset[i], moveId: learnset[i + 1]})
+  for (let i = 0; i < learnset.length; i += 2) {
+    moveList.push({ level: learnset[i], moveId: learnset[i + 1] });
   }
 
   const tmLearnset = pokemonInfo.tmLearnset;
@@ -26,23 +25,30 @@ export default function PokedexFeatures(props) {
 
   return (
     <div>
-      <div className='container'>
-        <div className='row'>
-          <Typography variant="h2" component="h3" sx={{ paddingLeft: '16px' }}>{pokemonInfo.name}</Typography>
+      <div className="container">
+        <div className="row">
+          <Typography variant="h2" component="h3" sx={{ paddingLeft: '16px' }}>
+            {pokemonInfo.name}
+          </Typography>
         </div>
       </div>
-      <div className='container'>
-        <div className='row'>
-          <div className='col col-4'>
-            <Avatar alt={pokemonInfo.name} src={pokemonInfo.imageSrc} sx={{ width: 80, height: 80, textAlign: 'center', margin: '16px' }}></Avatar>
+      <div className="container">
+        <div className="row">
+          <div className="col col-4">
+            <Avatar
+              alt={pokemonInfo.name}
+              src={pokemonInfo.imageSrc}
+              sx={{ width: 80, height: 80, textAlign: 'center', margin: '16px' }}
+            ></Avatar>
           </div>
-          <div className='col col-4'>
+          <div className="col col-4">
             <Type type1={pokemonInfo.type1} type2={pokemonInfo.type2} />
           </div>
-          <div className='col col-4'>
+          <div className="col col-4">
             <Typography variant="h6" component="h6">
               <p className={style.flex}>Size:</p>
-              {pokemonInfo.height}m, {pokemonInfo.weight}kg<br />
+              {pokemonInfo.height}m, {pokemonInfo.weight}kg
+              <br />
               <span style={{ fontSize: '0.8rem' }}>
                 <i>Grass Knot: {pokemonInfo.grassKnotPower}</i>
               </span>
@@ -50,18 +56,42 @@ export default function PokedexFeatures(props) {
           </div>
         </div>
       </div>
-      <div className="container" style={{ display: 'flex', marginTop: '25px'}}>
-        <BaseStatsList stats={pokemonInfo.baseStats} total={pokemonInfo.baseStatsTotal} />
-        <BaseStatCalculations stats={pokemonInfo.baseStats} xs={6} />
-      </div>
-      <div className='container'>
+      <PokemonStats baseStats={pokemonInfo.baseStats} baseStatsTotal={pokemonInfo.baseStatsTotal} />
+      <div className="container">
         <EvolutionGraph dexId={props.dexId} />
       </div>
       <Grid item xs={12}>
-        <PokedexAccordion dexId={props.dexId} learnset={moveList} panelId="panel1" headerId="panel1bh-header" ariaLabel="panel1bh-content" expanded={expanded} handleChange={handleChange} learnsetName="Moves learnt via level-up" />
-        <PokedexAccordion dexId={props.dexId} learnset={tmLearnset} panelId="panel2" headerId="panel2bh-header" ariaLabel="panel2bh-content" expanded={expanded} handleChange={handleChange} learnsetName="Moves learnt via Technical Machine" />
-        <PokedexAccordion dexId={props.dexId} learnset={eggLearnset} panelId="panel3" headerId="panel3bh-header" ariaLabel="panel3bh-content" expanded={expanded} handleChange={handleChange} learnsetName="Moves learnt via breeding" />
+        <PokedexAccordion
+          dexId={props.dexId}
+          learnset={moveList}
+          panelId="panel1"
+          headerId="panel1bh-header"
+          ariaLabel="panel1bh-content"
+          expanded={expanded}
+          handleChange={handleChange}
+          learnsetName="Moves learnt via level-up"
+        />
+        <PokedexAccordion
+          dexId={props.dexId}
+          learnset={tmLearnset}
+          panelId="panel2"
+          headerId="panel2bh-header"
+          ariaLabel="panel2bh-content"
+          expanded={expanded}
+          handleChange={handleChange}
+          learnsetName="Moves learnt via Technical Machine"
+        />
+        <PokedexAccordion
+          dexId={props.dexId}
+          learnset={eggLearnset}
+          panelId="panel3"
+          headerId="panel3bh-header"
+          ariaLabel="panel3bh-content"
+          expanded={expanded}
+          handleChange={handleChange}
+          learnsetName="Moves learnt via breeding"
+        />
       </Grid>
     </div>
-  )
+  );
 }
