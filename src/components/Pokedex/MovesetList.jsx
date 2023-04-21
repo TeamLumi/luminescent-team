23697815c561e8a5c9f1af0358bf793e0c0116f7
Phuc Tran <glup3.tran@gmail.com>
2023-04-21 11:17:17 +0200

    add more breakpoint stylings

--- src/components/Pokedex/MovesetList.jsx
+++ src/components/Pokedex/MovesetList.jsx
@@ -29,6 +29,8 @@ const TYPE_COLOR_MAP = {
   17: { name: 'Fairy', color: '#eb92e4' },
 };
 
+const responsiveFontSize = { fontSize: { xs: '0.7rem', sm: '0.8rem', md: '1rem' } };
+
 // interface props {
 //   moveset: {
 //     level: 'egg' | 'tm' | number;
@@ -48,7 +50,7 @@ export const MovesetList = ({ moveset }) => {
 
 const MoveIcon = ({ moveIconType }) => {
   if (typeof moveIconType === 'number') {
-    return <Typography textAlign="center">{moveIconType}</Typography>;
+    return <Typography sx={{ textAlign: 'center', ...responsiveFontSize }}>{moveIconType}</Typography>;
   }
 
   if (moveIconType === 'egg') {
@@ -66,61 +68,68 @@ const MovesetListItem = ({ moveLevel, moveId }) => {
   const move = getMoveProperties(moveId);
 
   return (
-    <ListItem>
-      <Box
-        sx={{
-          display: 'grid',
-          gridTemplateColumns: {
-            xs: '35px 150px 92px 64px 50px 70px 24px',
-            sm: '35px 150px 92px 64px 50px 70px 24px 1fr',
-          },
-          gridTemplateAreas: {
-            xs: '"icon name type dmg_type power accuracy pp"',
-            sm: '"icon name type dmg_type power accuracy pp desc"',
-          },
-          alignItems: 'center',
-          columnGap: '24px',
-        }}
-      >
-        <Box gridArea="icon">
-          <MoveIcon moveIconType={moveLevel} />
-        </Box>
-        <Box gridArea="name">
-          <Typography>{move.name}</Typography>
-        </Box>
-        <Box gridArea="type">
-          <PokemonMoveType typeColor={TYPE_COLOR_MAP[move.type].color} typeName={TYPE_COLOR_MAP[move.type].name} />
-        </Box>
-        <Box gridArea="dmg_type">
-          <img
-            src={DMG_TYPE_ICONS[move.damageType]}
-            style={{ float: 'right' }}
-            width="64px"
-            height="36px"
-            alt="Damage Type"
-          />
-        </Box>
-        <Box display="flex" flexDirection="column" alignItems="center" gridArea="power">
-          {move.power > 0 && (
-            <>
-              <Typography>Power</Typography>
-              <Typography>{move.power}</Typography>
-            </>
-          )}
-        </Box>
-        <Box display="flex" flexDirection="column" alignItems="center" gridArea="accuracy">
-          <Typography>Accuracy</Typography>
-          <Typography>{move.accuracy === 101 ? '--' : move.accuracy}</Typography>
-        </Box>
-        <Box display="flex" flexDirection="column" alignItems="center" gridArea="pp">
-          <Typography>PP</Typography>
-          <Typography>{move.maxPP}</Typography>
-        </Box>
-        <Box sx={{ display: { xs: 'none', sm: 'block' } }} gridArea="desc">
-          <Typography>{move.desc}</Typography>
-        </Box>
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: {
+          xs: '18px 92px 56px 48px 50px 50px 24px',
+          sm: '35px 105px 80px 56px 50px 50px 24px 1fr',
+          md: '35px 120px 30px 80px 64px 56px 72px 30px 500px',
+        },
+        gridTemplateAreas: {
+          xs: '"icon name type dmg_type power accuracy pp"',
+          sm: '"icon name type dmg_type power accuracy pp desc"',
+          md: '"icon name . type dmg_type power accuracy pp desc"',
+        },
+        alignItems: 'center',
+        columnGap: {
+          xs: '2px',
+          sm: '6px',
+        },
+        marginBottom: '8px',
+      }}
+    >
+      <Box gridArea="icon">
+        <MoveIcon moveIconType={moveLevel} />
       </Box>
-    </ListItem>
+
+      <Box gridArea="name">
+        <Typography sx={{ fontWeight: 700, ...responsiveFontSize }}>{move.name}</Typography>
+      </Box>
+
+      <Box gridArea="type">
+        <PokemonMoveType typeColor={TYPE_COLOR_MAP[move.type].color} typeName={TYPE_COLOR_MAP[move.type].name} />
+      </Box>
+
+      <Box gridArea="dmg_type" display="flex" alignItems="center">
+        <img src={DMG_TYPE_ICONS[move.damageType]} alt="Damage Type" />
+      </Box>
+
+      <Box display="flex" flexDirection="column" alignItems="center" gridArea="power">
+        {move.power > 0 && (
+          <>
+            <Typography sx={{ fontStyle: 'italic', ...responsiveFontSize }}>Power</Typography>
+            <Typography sx={{ ...responsiveFontSize }}>{move.power}</Typography>
+          </>
+        )}
+      </Box>
+
+      <Box display="flex" flexDirection="column" alignItems="center" gridArea="accuracy">
+        <Typography sx={{ fontStyle: 'italic', ...responsiveFontSize }}>Accuracy</Typography>
+        <Typography sx={{ ...responsiveFontSize }}>{move.accuracy === 101 ? '--' : move.accuracy}</Typography>
+      </Box>
+
+      <Box display="flex" flexDirection="column" alignItems="center" gridArea="pp">
+        <Typography sx={{ fontStyle: 'italic', ...responsiveFontSize }}>PP</Typography>
+        <Typography sx={{ ...responsiveFontSize }}>{move.maxPP}</Typography>
+      </Box>
+
+      <Box sx={{ display: { xs: 'none', sm: 'block' } }} gridArea="desc">
+        <Typography variant="body1" sx={{ ...responsiveFontSize }}>
+          {move.desc}
+        </Typography>
+      </Box>
+    </Box>
   );
 };
 
@@ -130,26 +139,31 @@ const PokemonMoveType = ({ typeName, typeColor }) => {
       sx={{
         padding: '4px',
         background: typeColor,
-        textAlign: 'center',
-        lineHeight: 1.25,
-        fontWeight: 700,
         borderColor: 'hsl(0 0% 0% / 0.2)',
         borderStyle: 'solid',
         borderRadius: '0.25rem',
-        textShadow:
-          '0 1px 0 #000,0 0 1px rgba(0,0,0,.6),0 0 2px rgba(0,0,0,.7),0 0 3px rgba(0,0,0,.8),0 0 4px rgba(0,0,0,.9)',
       }}
     >
-      <Box
+      <Typography
         sx={{
           color: '#fff',
           whiteSpace: 'nowrap',
           overflow: 'hidden',
           textOverflow: 'ellipsis',
+          lineHeight: 1.25,
+          textAlign: 'center',
+          fontWeight: 700,
+          textShadow:
+            '0 1px 0 #000,0 0 1px rgba(0,0,0,.6),0 0 2px rgba(0,0,0,.7),0 0 3px rgba(0,0,0,.8),0 0 4px rgba(0,0,0,.9)',
+          fontSize: {
+            xs: '0.6rem',
+            sm: '0.7rem',
+            md: '0.9rem',
+          },
         }}
       >
         {typeName}
-      </Box>
+      </Typography>
     </Box>
   );
 };
