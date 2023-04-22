a8ba26a3873846da969e7a31a57de37ab11378bf
Denmark14 <95157709+Denmark14@users.noreply.github.com>
2023-04-22 15:29:51 +0100

    More necessary functions

--- src/utils/dex/functions.js
+++ src/utils/dex/functions.js
@@ -21,4 +21,25 @@ function getGender(sex) {
   return null;
 }
 
+function parseTmLearnsetSection(decimal) {
+  return (decimal >>> 0).toString(2).split('').reverse().join('').padStart(32, 0);
+}
+
+function getImage(monsno = 0) {
+  return `/img/pm${monsno.toString().padStart(4, 0) ?? '0000'}_00_00_00_L.webp`;
+}
+
+function formatBaseStats(p) {
+  return `HP: ${p.basic_hp} / ATK: ${p.basic_atk} / DEF: ${p.basic_def} / SPA: ${p.basic_spatk} / SPD: ${p.basic_spdef} / SPE: ${p.basic_agi}`;
+}
+
+function getGrassKnotPower(weightkg) {
+  if (weightkg >= 200) return 120;
+  if (weightkg >= 100) return 100;
+  if (weightkg >= 50) return 80;
+  if (weightkg >= 25) return 60;
+  if (weightkg >= 10) return 40;
+  return 20;
+}
+
 export { FORM_MAP, getPokemonIdFromFormMap, getGender };
