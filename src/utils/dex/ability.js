0de52c1365bc65dd26c072fdb78bfb69489edf72
Denmark14 <95157709+Denmark14@users.noreply.github.com>
2023-04-22 14:47:48 +0100

    Ability utils

---
+++ src/utils/dex/ability.js
@@ -0,0 +1,24 @@
+const { abilityNames } = require('../../../__gamedata');
+
+function makeSmogonAbilityObject(abilityId = 0) {
+  const abilitiyString = abilityNames.labelDataArray[abilityId].wordDataArray[0].str;
+
+  return { 0: abilitiyString };
+}
+
+function getAbilityIdFromAbilityName(abilityString) {
+  if (!abilityString) return -1;
+  const abilityId = abilityNames.labelDataArray.findIndex((e) => e.wordDataArray[0].str === abilityString);
+  if (abilityId === -1) throw Error(`Bad ability name: ${abilityString}`);
+  return abilityId;
+}
+
+function getAbilityString(abiltiyId = 0) {
+  const abilityString = abilityNames.labelDataArray[abiltiyId].wordDataArray[0].str;
+  if (!abilityString) {
+    console.warn(abilityString, abiltiyId);
+  }
+  return abilityString;
+}
+
+export { makeSmogonAbilityObject, getAbilityIdFromAbilityName, getAbilityString };
