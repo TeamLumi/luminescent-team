e47e84a14a10aeaded268a83d837b34e78bb83b6
Phuc Tran <glup3.tran@gmail.com>
2023-04-23 20:07:04 +0200

    style alternative pokemon form section

--- src/components/Pokedex/PokemonAlternativeFormsList.jsx
+++ src/components/Pokedex/PokemonAlternativeFormsList.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
+import { Box, Container, Typography } from '@mui/material';
 import { POKEMON_FORM_ID_MAP, getPokemonFormIndexById, getPokemonImageFilename } from '../../core/pokemonFormSelector';
-import { Box, Typography } from '@mui/material';
+import { ImageWithFallback } from '../common/ImageWithFallback';
 
 export const PokemonAlternativeFormsList = ({ pokemonDexId }) => {
-  const alternativeForms = POKEMON_FORM_ID_MAP[pokemonDexId].slice(1);
+  const allPokemonForms = POKEMON_FORM_ID_MAP[pokemonDexId].map((form) => {
+    const formIndex = getPokemonFormIndexById(pokemonDexId, form.pokemonId);
+    const formFilename = getPokemonImageFilename(pokemonDexId, formIndex);
 
-  return (
+    return {
+      formName: form.formName,
+      formFilename: formFilename,
+    };
+  });
+
+  return allPokemonForms.length > 1 ? (
     <Box>
       <Typography fontSize="2rem">Alternative Forms</Typography>
-      <Box>
-        {alternativeForms.map((form) => {
-          const formIndex = getPokemonFormIndexById(pokemonDexId, form.pokemonId);
-          const formFilename = getPokemonImageFilename(pokemonDexId, formIndex);
-          return <img key={form.formName} src={`/img/${formFilename}`} />;
-        })}
-      </Box>
+      <Container>
+        <Box display="flex" flexWrap="wrap">
+          {allPokemonForms.map((form, i) => {
+            return (
+              <Box key={form.formName} display="flex" alignItems="center" margin="4px 16px 4px 4px">
+                <ImageWithFallback
+                  src={`/img/${form.formFilename}`}
+                  fallbackSrc={`/img/${allPokemonForms[0].formFilename}`}
+                  width={30}
+                  height={30}
+                />
+                <Typography marginLeft="8px">
+                  {form.formName}
+                  {i < allPokemonForms.length - 1 && ','}
+                </Typography>
+              </Box>
+            );
+          })}
+        </Box>
+      </Container>
     </Box>
-  );
+  ) : null;
 };
