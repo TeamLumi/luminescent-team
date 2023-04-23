4ad68fad3afd9d1b9703a2fa1df870c7ba8431bf
Phuc Tran <glup3.tran@gmail.com>
2023-04-23 15:22:26 +0200

    add junit test report

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('jest').Config} */
 const config = {
-  reporters: ['default', ['jest-junit', { outputDirectory: 'reports', outputName: 'report.xml' }]],
+  reporters: ['default', ['jest-junit', { outputDirectory: 'reports', outputName: 'jest-report.xml' }]],
 };
 
 module.exports = config;
