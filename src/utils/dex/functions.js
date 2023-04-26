607432c897f5c338031de217449649b324b304d4
Denmark14 <95157709+Denmark14@users.noreply.github.com>
2023-04-26 16:38:37 +0100

    Remove unused imports

--- src/utils/dex/functions.js
+++ src/utils/dex/functions.js
@@ -1,8 +1,4 @@
 import { PersonalTable } from '../../../__gamedata';
-import { getPokemonName } from './name';
-import { getAbilityString } from './ability';
-import { getWeight, getHeight } from './details';
-import { getTypeName } from './types';
 
 //BDSP does not stick to the same structure when working with forms, thus this map is necessary.
 const FORM_MAP = PersonalTable.Personal.reduce(createFormMap, {});
