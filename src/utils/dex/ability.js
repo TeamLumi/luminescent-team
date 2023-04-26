e4dbff4a9a8c55ce9e2c5c9cb3b58df128cb1751
Denmark14 <95157709+Denmark14@users.noreply.github.com>
2023-04-26 13:47:48 +0100

    Better handling of bad params in ability

--- src/utils/dex/ability.js
+++ src/utils/dex/ability.js
@@ -1,8 +1,8 @@
 const { abilityNames, abilityInfo } = require('../../../__gamedata');
 
 function makeSmogonAbilityObject(abilityId = 0) {
-  const abilitiyString = abilityNames.labelDataArray[abilityId].wordDataArray[0].str;
-
+  const abilitiyString = abilityNames.labelDataArray[abilityId]?.wordDataArray[0]?.str ?? null;
+  if (abilitiyString === null) return null;
   return { 0: abilitiyString };
 }
 
@@ -14,15 +14,21 @@ function getAbilityIdFromAbilityName(abilityString) {
 }
 
 function getAbilityString(abiltiyId = 0) {
-  const abilityString = abilityNames.labelDataArray[abiltiyId].wordDataArray[0].str;
+  const abilityString = abilityNames.labelDataArray[abiltiyId]?.wordDataArray[0]?.str ?? null;
   if (!abilityString) {
-    console.warn(abilityString, abiltiyId);
+    return null;
   }
   return abilityString;
 }
 
 function getAbilityInfo(id) {
-  return abilityInfo.labelDataArray[id]?.wordDataArray[0]?.str || 'None';
+  const wordData = abilityInfo.labelDataArray[id]?.wordDataArray;
+  if (wordData === null || wordData === undefined || wordData.length === 0) return 'None';
+  let description = wordData.reduce((abilityDescription, currentString) => {
+    return abilityDescription + currentString.str + ' ';
+  }, '');
+
+  return description.trim();
 }
 
 export { makeSmogonAbilityObject, getAbilityIdFromAbilityName, getAbilityString, getAbilityInfo };
