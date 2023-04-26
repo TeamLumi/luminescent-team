5c7802fbf7f0b1cac4c1456e42305275177e1682
Denmark14 <95157709+Denmark14@users.noreply.github.com>
2023-04-26 14:01:26 +0100

    Detail tests

---
+++ src/utils/dex/details.test.js
@@ -0,0 +1,33 @@
+import { getHeight, getWeight } from './details';
+
+const TEST_POKEMON = 1; //Bulbasaur
+
+describe('Dex details getters', () => {
+  it('Should return a valid height in metric format', () => {
+    const height = getHeight(TEST_POKEMON);
+    const desiredResult = '0.71';
+    expect(typeof height).toBe('string');
+    expect(height).toBe(desiredResult);
+  });
+
+  it('Should return a height of 0 if an invalid pokemon ID is supplied', () => {
+    const height = getHeight(-1);
+    const desiredResult = '0';
+    expect(typeof height).toBe('string');
+    expect(height).toBe(desiredResult);
+  });
+
+  it('Should return a valid weight in metric format', () => {
+    const weight = getWeight(TEST_POKEMON);
+    const desiredResult = '6.89';
+    expect(typeof weight).toBe('string');
+    expect(weight).toBe(desiredResult);
+  });
+
+  it('Should return a weight of 0 if an invalid pokemon ID is supplied', () => {
+    const weight = getWeight(-1);
+    const desiredResult = '0';
+    expect(typeof weight).toBe('string');
+    expect(weight).toBe(desiredResult);
+  });
+});
