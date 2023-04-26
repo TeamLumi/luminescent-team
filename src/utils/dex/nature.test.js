28450ad06edae781f964036e54c217253fa5346f
Denmark14 <95157709+Denmark14@users.noreply.github.com>
2023-04-26 16:20:12 +0100

    nature tests

---
+++ src/utils/dex/nature.test.js
@@ -0,0 +1,32 @@
+import { getNatureId, getNatureName } from './nature';
+
+describe('Dex utils Nature getters', () => {
+  describe('getNatureId', () => {
+    test('should return -1 if the input is falsy', () => {
+      const natureString = '';
+      const actual = getNatureId(natureString);
+      expect(actual).toBe(-1);
+    });
+
+    test('should throw an error if the input is not a valid nature string', () => {
+      const natureString = 'Bad Nature';
+      expect(() => getNatureId(natureString)).toThrow(`Bad natureString: ${natureString}`);
+    });
+
+    test('should return the correct nature ID for a valid nature string', () => {
+      const natureString = 'Hardy';
+      const expected = 0;
+      const actual = getNatureId(natureString);
+      expect(actual).toBe(expected);
+    });
+  });
+
+  describe('getNatureName', () => {
+    test('should return the correct nature name for a given nature ID', () => {
+      const natureId = 5;
+      const expected = 'Bold';
+      const actual = getNatureName(natureId);
+      expect(actual).toBe(expected);
+    });
+  });
+});
