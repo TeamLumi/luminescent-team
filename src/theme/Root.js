699327b1af690b4927a576dc964fe0d28d581c34
Phuc Tran <glup3.tran@gmail.com>
2023-04-28 12:36:14 +0200

    properly intergrate MUI with docusaurus

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,5 +1,13 @@
-import React from "react";
-// Default implementation, that you can customize
+import React from 'react';
+import { getInitColorSchemeScript } from '@mui/material';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material';
+
+// Integrating MUI with docusuraus: https://webreaper.dev/blog/material-ui-theme-with-docusaurus/
 export default function Root({ children }) {
-  return <>{children}</>;
+  return (
+    <>
+      {getInitColorSchemeScript()}
+      <CssVarsProvider>{children}</CssVarsProvider>
+    </>
+  );
 }
