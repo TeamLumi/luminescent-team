518af7668f16a2b83b91e5e5cce39381f0709977
Phuc Tran <glup3.tran@gmail.com>
2023-05-27 23:18:58 +0200

    update project name for github actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'TeamLumi', // Usually your GitHub org/user name.
-  projectName: 'luminescent-team', // Usually your repo name.
+  projectName: 'luminescent-team-dev', // Usually your repo name.
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
