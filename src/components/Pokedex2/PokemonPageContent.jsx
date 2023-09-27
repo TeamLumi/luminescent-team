7145118d6ae3b730163297a58fd8f8f46a0d92ab
Denmark14 <95157709+Denmark14@users.noreply.github.com>
2023-09-27 02:23:06 +0100

    License updates

--- src/components/Pokedex2/PokemonPageContent.jsx
+++ src/components/Pokedex2/PokemonPageContent.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './styles.module.css';
-import { Box, Typography, Container } from '@mui/material';
+import { Box, Typography, Container, Button, Modal } from '@mui/material';
 import Type from './type';
 import EvolutionGraph from './EvolutionGraph';
 import { PokemonStats } from './PokemonStats';
@@ -14,11 +14,52 @@ import { PokemonEggGroups } from './PokemonEggGroups';
 import { ImageWithFallback } from '../common/ImageWithFallback';
 
 export const PokemonPageContent = ({ pokemon, pokemonNames }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Container>
       <Container>
         <Box display="flex" justifyContent="center" marginTop="16px">
           <PokemonSearchBox pokemonNames={pokemonNames} pokemonId={pokemon.id} />
+          <Button variant="contained" onClick={handleOpen}>?</Button>
+          <Modal open={open} onClose={handleClose}>
+            <Box style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              backgroundColor: 'var(--ifm-color-content-inverse)',
+              color: 'var(--ifm-color-content)',
+              padding: '16px',
+              borderRadius: '8px',
+              border: 'var(--ifm-table-border-width) solid var(--ifm-table-border-color)',
+            }}
+            >
+              <Typography variant="h6">
+This site documents all Pokémon available in Luminescent Platinum 2.0F, including their types, abilities, stats, evolution methods, forms, wild held items, and movesets. It currently does not include locations, but you may use our <a href="https://docs.google.com/spreadsheets/d/1a-NSfEgtt8kAr1cXwKkmY2SylYMs2tUG5tMSIhK0-OY/edit">Pokémon Locations by Route documentation</a> or the Pokédex channel in our <a href="https://discord.gg/luminescent">Discord</a> to access that information in the meanwhile.
+<br/><br/>
+For your convenience:
+<ul>
+  <li>Clicking on an Ability will list its effects on the same page.</li>
+  <li>Clicking on item rates will also show you what they change to when a Pokémon with Frisk or Super Luck are at the front of the party.</li>
+</ul>
+Planned Changes:
+<ul>
+  <li>Encounter locations to the Pokédex displayed here.</li>
+  <li>A comparison toggle to view vanilla BDSP stats to against any changes we have made.</li>
+</ul>
+Please keep an eye on this main page to see what other features may become available.
+</Typography>
+            </Box>
+          </Modal>
         </Box>
       </Container>
       <div className="container">
@@ -93,6 +134,19 @@ export const PokemonPageContent = ({ pokemon, pokemonNames }) => {
         <PokemonAccordion title="Moves learnt via Tutor" id="eggMoveset">
           <PokemonMovesetList moveset={pokemon.learnset.tutor} movesetPrefix="tutor" pokemonDexId={pokemon.id} />
         </PokemonAccordion>
+      </Container>
+
+      <Container>
+        <Box style={{marginTop: '50px', marginBottom: '10px'}}>
+          <Typography variant="overline">
+            <b>Credits:</b>
+          </Typography>
+          <Typography variant="caption">
+            Design based on the application by the <a href="https://github.com/Zarel/Pokemon-Showdown-Dex/graphs/contributors">Smogon Dex Team</a>, which can be found <a href="https://dex.pokemonshowdown.com/">here.</a><br/>
+            Specific credits for the full Pokédex application go to Denmark, Glup and A-A-ron from Team Luminescent. Smogon Dex Team credits are as follows:<br/> Zarel, Marty-D, scheibo, KrisXV, Karthik99999, AnnikaCodes, SadisticMystic, pyuk-bot, mia-pi-git, penpexgit, asgdf and jakobw.<br/>
+            This is the only section of the site where external inspiration was used.
+          </Typography>
+        </Box>
       </Container>
     </Container>
   );
