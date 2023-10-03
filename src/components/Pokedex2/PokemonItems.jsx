ba3bcdb2f0fb1e5fd342ef9df95c628677c09bd0
aarcon <bowtiesarecool110@gmail.com>
2023-10-02 22:15:17 -0600

    Add Modal for how ability changes item percentages

--- src/components/Pokedex2/PokemonItems.jsx
+++ src/components/Pokedex2/PokemonItems.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Typography, Modal } from '@mui/material';
 import { getPokemon } from "../../../plugins/pokedex-data-plugin/dex/pokemon";
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import { getItemImageUrl } from '../../../plugins/pokedex-data-plugin/dex/item';
 
 export const PokemonItems = ({ pokemonId }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const pokeInfo = getPokemon(pokemonId)
   const item1 = pokeInfo.item1
   const item2 = pokeInfo.item2
@@ -18,7 +26,16 @@ export const PokemonItems = ({ pokemonId }) => {
       <Box display="grid" gridTemplateColumns="repeat(5, 1fr)" gap={1}>
         <>
           <Box gridColumn="span 5">
-            <Typography sx={{ fontWeight: 800, fontSize: '0.8rem' }}>Wild Held Items:</Typography>
+            <Typography
+              sx={{
+                textDecoration: (!allItems && !noItems) ? 'underline' : "",
+                fontWeight: 800,
+                fontSize: '0.8rem'
+              }}
+              onClick={handleOpen}
+            >
+              Wild Held Items:
+            </Typography>
           </Box>
         </>
         {(item1 !== "None" && !allItems) && (<ItemContainer item={item1} percentage={50} />)}
@@ -34,18 +51,45 @@ export const PokemonItems = ({ pokemonId }) => {
         {allItems && (
           <ItemContainer item={item1} percentage={100} />
         )}
+        {(!allItems && !noItems) && (
+          <Modal open={open} onClose={handleClose}>
+            <Box style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              backgroundColor: 'var(--ifm-color-content-inverse)',
+              color: 'var(--ifm-color-content)',
+              padding: '16px',
+              borderRadius: '8px',
+              border: 'var(--ifm-table-border-width) solid var(--ifm-table-border-color)',
+            }}
+            >
+              <Box display="grid" gridTemplateColumns="repeat(3)" gap={1}>
+                <Box gridColumn="span 3">
+                  <Typography >
+                    If the Lead Pokémon has Frisk,<br />
+                    Super Luck or Compound Eyes:</Typography>
+                </Box>
+                {(item1 !== "None" && !allItems) && (<ItemContainer item={item1} percentage={60} span='span 1' />)}
+                {(item3 !== "None" && !allItems) && (<ItemContainer item={item3} percentage={20} span='span 1' />)}
+                {(item2 !== "None" && !allItems) && (<ItemContainer item={item2} percentage={20} span='span 1' />)}
+              </Box>
+            </Box>
+          </Modal>
+        )}
       </Box>
     </div>
   );
 };
 
-const ItemContainer = ({item, percentage}) => {
+const ItemContainer = ({item, percentage, span='span 2'}) => {
   return (
     <>
-      <Box gridColumn="span 2" display="flex" alignItems={"center"} sx={{marginLeft: "16px"}}>
+      <Box gridColumn={span} display="flex" alignItems={"center"} sx={{marginLeft: "16px"}}>
         <Typography >{percentage}%: {item}</Typography>
       </Box>
-      <Box gridColumn="span 2">
+      <Box gridColumn={span}>
         <img
           key={item}
           src={useBaseUrl(`${getItemImageUrl(item)}`)}
