e0f9c8c43bfc563630ff31edfcdb12ea8d95bbcb
aarcon <bowtiesarecool110@gmail.com>
2023-11-17 15:47:12 -0700

    Requested changes, refactor highlights to new file

--- src/components/Mapper/SettingsModal.jsx
+++ src/components/Mapper/SettingsModal.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { Modal, Button, TextField, Typography, Grid, Box } from '@mui/material';
-import {useColorMode} from '@docusaurus/theme-common';
+import { Modal, Button, Typography, Box } from '@mui/material';
 import './style.css';
+import { ChangeHighlightColors } from './HighlightColors';
 
 const SettingsModal = ({ colors, setColors, showModal, onHide }) => {
-  const {colorMode, setColorMode} = useColorMode();
   const [newColors, setNewColors] = useState(colors);
 
   const handleChange = (colorKey, subKey, value) => {
@@ -32,49 +31,17 @@ const SettingsModal = ({ colors, setColors, showModal, onHide }) => {
   return (
     <Modal open={showModal} onClose={handleNoSaveClose}>
       <Box className='modalBox' sx={{ padding: '16px' }}>
-        <Box boxShadow={colorMode === 'dark' ? 0 : 4} p={2} bgcolor="background.main">
+        <Box boxShadow={4} p={2} bgcolor="background.main">
           <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '16px' }}>
-            <Typography variant="h6" sx={{ width: '90%' }}>Edit Colors</Typography>
+            <Typography variant="h6" sx={{ width: '90%' }}>Settings</Typography>
             <Button onClick={handleNoSaveClose} color='error' variant='contained'>X</Button>
           </div>
         </Box>
-        <form>
-          <Grid container spacing={3}>
-            {colorKeys.map((colorKey) => (
-              <Grid item xs={12} sm={4} key={colorKey}>
-                <Typography variant="subtitle1" style={{ marginTop: '16px' }}>
-                  {`${colorKey.toUpperCase()} Color`}
-                </Typography>
-                <div
-                  style={{
-                    width: '20px',
-                    height: '20px',
-                    marginLeft: '8px',
-                    backgroundColor: `rgba(${newColors[colorKey].r},${newColors[colorKey].g},${newColors[colorKey].b},${newColors[colorKey].a})`,
-                    border: '1px solid #fff',
-                  }}
-                />
-                {Object.keys(newColors[colorKey]).map((subKey) => (
-                  <div key={subKey} style={{ marginTop: '8px' }}>
-                    <TextField
-                      label={subKey.toUpperCase()}
-                      type="number"
-                      variant="outlined"
-                      size="small"
-                      inputProps={{
-                        min: subKey === 'a' ? '0' : '0',
-                        max: subKey === 'a' ? '1' : '255',
-                        step: subKey === 'a' ? '0.1' : '1',
-                      }}
-                      value={newColors[colorKey][subKey]}
-                      onChange={(e) => handleChange(colorKey, subKey, e.target.value)}
-                    />
-                  </div>
-                ))}
-              </Grid>
-            ))}
-          </Grid>
-        </form>
+        <ChangeHighlightColors
+          colorKeys={colorKeys}
+          newColors={newColors}
+          handleChange={handleChange}
+        />
         <div style={{ marginTop: '16px', display: 'flex', justifyContent: 'start' }}>
           <Button variant="contained" color="secondary" onClick={handleClose}>
             Save Changes
