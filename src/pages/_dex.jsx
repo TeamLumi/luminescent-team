b34ffa464e2b13bc3a5917afcde4c8858cd20457
AarCon <bowtiesarecool110@gmail.com>
2023-11-28 23:02:45 -0700

    Add meta image for pokemon pages

--- src/pages/_dex.jsx
+++ src/pages/_dex.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import { PokemonPageContent } from '../components/Pokedex2/PokemonPageContent';
+import Head from '@docusaurus/Head';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function PokemonPage({ pokemon, pokemonList }) {
   // required for webpack SSR
@@ -8,11 +10,20 @@ export default function PokemonPage({ pokemon, pokemonList }) {
     return null;
   }
 
+  const metaImage = useBaseUrl(`/img/${pokemon.imageSrc}`) || useBaseUrl(`/img/${pokemon.forms[0].imageSrc}`)
+
   return (
     <Layout
       title={pokemon.name}
       description="Pokémon Luminescent Version, A ROM Hack for Pokémon Brilliant Diamond and Shining Pearl"
     >
+      <Head>
+        <meta
+          name="Pokémon Luminescent Version"
+          property='og:image'
+          content={metaImage}
+        />
+      </Head>
       <PokemonPageContent pokemon={pokemon} pokemonNames={pokemonList} />
     </Layout>
   );
