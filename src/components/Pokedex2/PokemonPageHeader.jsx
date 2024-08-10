33554f893b3e7b0ecae949b070efd08ccae091d5
AarCon <bowtiesarecool110@gmail.com>
2024-08-10 13:10:29 -0600

    Maybe make the Headers work better???

--- src/components/Pokedex2/PokemonPageHeader.jsx
+++ src/components/Pokedex2/PokemonPageHeader.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Head from '@docusaurus/Head';
-import { useGlobalState } from '../common/GlobalState';
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { useGlobalState } from "../common/GlobalState";
+import { Helmet } from "react-helmet";
 
 const PokemonPageHeader = ({ pokemon, pokemon3 }) => {
-  const [globalState] = useGlobalState();
+  const [globalState, updateMode] = useGlobalState();
   const { siteConfig } = useDocusaurusContext();
   const pokemonInfo = globalState.mode === "2.0" ? pokemon : pokemon3;
   const metaImage = 
@@ -12,14 +12,17 @@ const PokemonPageHeader = ({ pokemon, pokemon3 }) => {
     `https://luminescent.team${pokemonInfo.forms[0].imageSrc}`;
 
   return (
-    <Head>
+    <Helmet>
+      <meta property="description" content={pokemonInfo.dexDescription} />
       <meta property="og:title" content={pokemonInfo.name} data-rh="true" />
+      <meta property="og:description" content={pokemonInfo.dexDescription} />
+      <meta property="og:image" itemProp="image primaryImageOfPage" content={metaImage} />
       <meta name="og:site_name" content={siteConfig.title} />
+      <meta property="twitter:title" content={pokemonInfo.name} data-rh="true" />
       <meta name="twitter:card" content="summary" />
-      <meta property='og:image' itemProp="image primaryImageOfPage" content={metaImage} />
-      <meta property='description' content={pokemonInfo.dexDescription} />
-      <meta property='og:description' content={pokemonInfo.dexDescription} />
-    </Head>
+      <meta property="twitter:description" content={pokemonInfo.dexDescription} />
+      <meta property="twitter:image" itemProp="image primaryImageOfPage" content={metaImage} />
+    </Helmet>
   );
 };
 
