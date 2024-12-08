50bdfd648225d305f3dddd7f8e919ab5d685e879
pedro <pedrogabrielcruzlima@gmail.com>
2024-12-08 00:33:58 -0300

    button to export trainer to pokemon show down for calcs in trainer details

--- src/components/Mapper/Trainers/TrainersModal.jsx
+++ src/components/Mapper/Trainers/TrainersModal.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Dialog, DialogContent, DialogTitle, IconButton } from "@mui/material"
+import { Dialog, DialogContent, DialogTitle, IconButton, Snackbar } from "@mui/material"
+import Button from '@mui/material/Button';  
 import CloseIcon from '@mui/icons-material/Close';
-
+import { getMoveProperties, getPokemonName } from '../../../utils/dex';
 import { Trainers } from "./Trainers";
 
 const TrainersModal = ({
@@ -13,6 +14,33 @@ const TrainersModal = ({
   const handleClose = () => {
     onHide();
   };
+  const [showSnackbar, setShowSnackbar] = React.useState(false);
+  const handleExport = () => {
+    const showdownMon = selectedTrainer.team.map(pokemon => transformToText(pokemon));
+    const exportText = showdownMon.join("\n\n");
+    navigator.clipboard.writeText(exportText).then(() => {
+      setShowSnackbar(true);
+    });
+
+  };
+
+  const handleCloseSnackbar = () => {
+    setShowSnackbar(false);
+  };
+
+  function transformToText(pokemon) {
+    const genderSymbol = pokemon.gender === "FEMALE" ? "(F)" : "(M)";
+    const moves = pokemon.moves.map(id => "- " + getMoveProperties(id).name).join("\n");
+    
+    return `${getPokemonName(pokemon.id)} ${genderSymbol} @ ${pokemon.item}
+Level: ${pokemon.level}
+${pokemon.nature} Nature
+Ability: ${pokemon.ability}
+EVs: ${pokemon.evhp} HP / ${pokemon.evatk} Atk / ${pokemon.evdef} Def / ${pokemon.evspatk} SpA / ${pokemon.evspeed} Spe
+IVs: ${pokemon.ivhp} HP / ${pokemon.ivatk} Atk / ${pokemon.ivdef} Def / ${pokemon.ivspatk} SpA / ${pokemon.ivspdef} SpD / ${pokemon.ivspeed} Spe
+${moves}`;
+  }
+
   return (
     <Dialog
       open={showModal}
@@ -33,13 +61,25 @@ const TrainersModal = ({
       >
         <CloseIcon />
       </IconButton>
-
+      <Button
+              variant="outlined"
+              sx={{ margin: "0.5rem 1rem" }}
+              onClick={handleExport}
+            >
+              Export to Showdown
+      </Button>
       <DialogContent dividers sx={{maxWidth: "1108px"}}>
         <Trainers
           pokemonList={pokemonList}
           selectedTrainer={selectedTrainer}
         />
       </DialogContent>
+      <Snackbar
+        open={showSnackbar}
+        autoHideDuration={3000}
+        onClose={handleCloseSnackbar}
+        message="Copied to clipboard successfully!"
+      />
     </Dialog>
   )
 };
